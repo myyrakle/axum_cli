20b7452deaeb4c91bff26d87a3cb7deea5bd517f
myyrakle <sssang97@naver.com>
2023-06-01 01:32:23 +0900

    init: main function

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,28 @@
 mod cli;
 
-fn main() {
-    println!("Hello, world!");
+use clap::Parser;
+use cli::{command::Command, Args};
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let args = Args::parse();
+
+    match args.command {
+        Command::New(command) => {
+            let project_name = command.project_name;
+            let template_name = command.option.template_name.unwrap_or("default".to_owned());
+            // let template_manager = TemplateManager::new(project_name.clone(), template_name);
+
+            // template_manager.new_template(project_name).await?;
+        }
+        Command::Init(command) => {
+            let project_name = command.project_name.unwrap_or("sample".to_owned());
+            let template_name = command.option.template_name.unwrap_or("default".to_owned());
+            // let template_manager = TemplateManager::new(project_name, template_name);
+
+            // template_manager.new_template(".".to_owned()).await?;
+        }
+    }
+
+    Ok(())
 }
