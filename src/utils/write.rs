3af066861f3c5dc627a5d2f0b9d90d500445bbe3
myyrakle <sssang97@naver.com>
2023-06-07 01:40:22 +0900

    fix: type error

--- src/utils/write.rs
+++ src/utils/write.rs
@@ -1,17 +1,8 @@
-<<<<<<< HEAD
-use std::path::Path;
-=======
 use std::{path::PathBuf, str::FromStr};
->>>>>>> 28705d68f528975c0aaecad9f5b03fafaec44b23
 
 use super::zip::ParsedZipData;
 
 pub async fn write_template(template: ParsedZipData, base_path: String) {
-<<<<<<< HEAD
-    if !Path::new(base_path.as_str()).exists() {
-        std::fs::create_dir(&base_path).unwrap();
-        println!(">>>>> {} >>> directory created", base_path);
-=======
     let base_path = PathBuf::from_str(base_path.as_str()).unwrap();
 
     if !base_path.exists() {
@@ -20,7 +11,6 @@ pub async fn write_template(template: ParsedZipData, base_path: String) {
             ">>>>> {} >>> directory created",
             base_path.to_str().unwrap()
         );
->>>>>>> 28705d68f528975c0aaecad9f5b03fafaec44b23
     }
 
     for (path, data) in template.into_iter() {
