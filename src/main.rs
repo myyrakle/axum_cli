533204e8d7d0626cfa39ae1febf3302562e66cb4
myyrakle <sssang97@naver.com>
2023-06-06 23:05:21 +0900

    add: router match

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,10 @@ mod run;
 mod utils;
 
 use clap::Parser;
-use cli::{command::Command, Args};
+use cli::{
+    command::{router::RouterCommand, Command},
+    Args,
+};
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -23,7 +26,19 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
             run::init::run_init(project_name, template_name).await;
         }
-        _ => {}
+        Command::Router(command) => {
+            match command.command {
+                RouterCommand::New(command) => {
+                    let router_name = command.router_name;
+                    let template_name = command.option.template_name.unwrap_or("crud".to_owned());
+
+                    println!("router_name: {}", router_name);
+                    println!("template_name: {}", template_name);
+
+                    //run::router::run_new_router(router_name, template_name).await;
+                }
+            }
+        }
     }
 
     Ok(())
